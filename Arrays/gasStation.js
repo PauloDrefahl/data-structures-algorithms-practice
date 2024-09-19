28d88e8f2cda0b6c4c92f392a7a61b464121780f
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-09-19 16:08:57 -0400

    new file structure

---
+++ Arrays/gasStation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    
+    for( i = 0; i < gas.length; i++){
+        console.log('test')
+    }
+};
+
+gas = [1,2,3,4,5]
+cost = [3,4,5,1,2]
+
+console.log(canCompleteCircuit(gas,cost))
+
---
+++ LinkedLists/ImplementsinglyLinkedList.js
@@ -0,0 +1,37 @@
+class Node{
+    constructor(val, next){
+        this.val = val
+        this.next = next
+    }
+}
+
+class SinglyLinkedList{
+    constructor (val, next){
+        this.head = {
+            val: val,
+            next: null
+        }
+        this.tail = this.head
+        this.len = 1
+    }
+
+    append(val){
+        let newNode = new Node(val)
+        this.head.next = newNode
+        this.tail = newNode
+        this.len++;
+    }
+
+    transverse(){
+        while(this.tail.next)
+    }
+
+}
+
+function main(){
+    linkedList = new SinglyLinkedList(10);
+    linkedList.append(20);
+
+}
+
+main()
