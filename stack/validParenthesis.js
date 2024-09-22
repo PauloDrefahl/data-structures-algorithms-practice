64a9a6bb35ed5fa75a19fbac66012f644befe148
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-09-22 15:44:12 -0400

    add valid Parentheses question

---
+++ stack/validParenthesis.js
@@ -0,0 +1,20 @@
+function isValid(s) {
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    const stack = [];
+    
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else if (char === ')' || char === '}' || char === ']') {
+            if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
