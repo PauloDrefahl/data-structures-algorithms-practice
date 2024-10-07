de3371fbcc9c4f687b4fcd3b1844ec99b33f5697
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-10-06 20:35:46 -0400

    Add question 2 10-6

---
+++ Arrays/4Sum.js
@@ -0,0 +1,30 @@
+function fourSum(nums, target) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        for (let j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            let left = j + 1, right = n - 1;
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
