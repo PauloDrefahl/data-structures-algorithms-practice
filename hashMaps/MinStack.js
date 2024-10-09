2fab1fd8aeef9e05706368674401a64699ee227d
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-10-09 15:39:14 -0400

    Add question  10-9

---
+++ hashMaps/MinStack.js
@@ -0,0 +1,27 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+        if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop() {
+        if (this.stack.pop() === this.minStack[this.minStack.length - 1]) {
+            this.minStack.pop();
+        }
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
