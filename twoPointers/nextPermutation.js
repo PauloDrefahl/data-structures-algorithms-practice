ecd21538f8916d4161ea614536110e88f39bd0f3
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-10-25 23:38:42 -0400

    add questions 10-25

---
+++ twoPointers/nextPermutation.js
@@ -0,0 +1,26 @@
+function nextPermutation(nums) {
+    let i = nums.length - 2;
+
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    reverse(nums, i + 1);
+}
+
+function reverse(nums, start) {
+    let end = nums.length - 1;
+    while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]];
+        start++;
+        end--;
+    }
+}
