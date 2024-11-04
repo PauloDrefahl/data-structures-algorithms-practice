8b565aafaa4f117a5bc2183e2bda89bd64c92b0e
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-11-04 13:01:43 -0500

    question 11-4-2

---
+++ Arrays/intToRoman.js
@@ -0,0 +1,14 @@
+var intToRoman = function(num) {
+    let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    let result = '';
+    
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
+        }
+    }
+    
+    return result;
+};
