77e1aa7fccd2c416625b69ac237b97cc3be229d3
PauloDrefahl <pmdrefahl5251@eagle.fgcu.edu>
2024-11-19 16:47:09 -0500

    Arrays Question 2 11-19

---
+++ Arrays/Q5-majorityElement.py
@@ -0,0 +1,13 @@
+#Question 5: we need tp just create a hasmap of the number of time that values appear. Then execite a max function with the key parameter to grab the key associated with that
+#Largest value. 
+
+class Solution(object):
+    def majorityElement(self, nums):
+        count = {}
+        for i in range(len(nums)):
+            if nums[i] in count:
+                count[nums[i]] += 1
+            else:
+                count[nums[i]] = 1
+        return max(count, key=count.get)
+        
