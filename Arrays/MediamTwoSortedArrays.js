454ccb6fa7294375123813435f6cd66f4ac374b4
paulodrefahl <pmdrefahl5251@eagle.fgcu.edu>
2025-01-15 12:00:00 -0500

    DS 2.1v

--- Arrays/MediamTwoSortedArrays.js
+++ Arrays/MediamTwoSortedArrays.js
@@ -1,4 +1,4 @@
-function findMedianSortedArrays(nums1, nums2) {
+function findMedianSortedArrays1(nums1, nums2) {
     if (nums1.length > nums2.length) {
         return findMedianSortedArrays(nums2, nums1);
     }
